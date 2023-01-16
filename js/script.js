856795bd884f2675b2f7dba3382ddbb806252623
tylkeen1803 <evghen.naydenko@gmail.com>
2023-01-16 22:25:45 +0200

    added carousel on native js

--- js/script.js
+++ js/script.js
@@ -308,12 +308,85 @@ window.addEventListener('DOMContentLoaded', () => {
 	const next = document.querySelector('.offer__slider-next');
 	const total = document.querySelector('#total');
 	const current = document.querySelector('#current');
-
-	//ин
+	const slidesWrapper = document.querySelector('.offer__slider-wrapper');
+	const slidesField = document.querySelector('.offer__slider-inner');
+	const width = window.getComputedStyle(slidesWrapper).width;
 	let slideIndex = 1;
+	let offset = 0;
 
+
+	slidesField.style.width = 100 * slides.length + '%';
+	slidesField.style.display = 'flex';
+	slidesField.style.transition = '0.5s all';
+	slidesWrapper.style.overflow = 'hidden';
+	slides.forEach(slide => {
+		slide.style.width = width;
+	});
+
+	//проверка и добавления нуля к однозначным числам
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+		current.textContent = `0${slideIndex}`;
+	} else {
+		total.textContent = slides.length;
+		current.textContent = slideIndex;
+	}
+
+	next.addEventListener('click', () => {
+		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+			offset = 0;
+		} else {
+			offset += +width.slice(0, width.length - 2);
+		}
+
+		slidesField.style.transform = `translateX(-${offset}px)`;
+
+		if (slideIndex == slides.length) {
+			slideIndex = 1;
+		}
+		else {
+			slideIndex++;
+		}
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+
+
+	});
+
+	prev.addEventListener('click', () => {
+		if (offset == 0) {
+			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+		} else {
+			offset -= +width.slice(0, width.length - 2);
+		}
+
+		slidesField.style.transform = `translateX(-${offset}px)`;
+
+		if (slideIndex == 1) {
+			slideIndex = slides.length;
+		}
+		else {
+			slideIndex--;
+		}
+
+		if (slides.length < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	});
+
+
+	// ---- simple slider way ---------
+
+	/* 
 	showSlides(slideIndex);
 
+	//проверка и добавления нуля к однозначным числам
 	if (slides.length < 10) {
 		total.textContent = `0${slides.length}`;
 	} else {
@@ -350,8 +423,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	next.addEventListener('click', () => {
 		plusSlides(1);
 	})
-
-
-
+ */
 
 });
