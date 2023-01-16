fa30072c3cd67510062fef1e8354d2bbcaa98448
tylkeen1803 <evghen.naydenko@gmail.com>
2023-01-16 19:33:30 +0200

    all work besides POST-request

--- js/script.js
+++ js/script.js
@@ -209,12 +209,11 @@ window.addEventListener('DOMContentLoaded', () => {
 	// }
 
 	const getResource = async (url) => {
-		const res = await fetch(url);
 
+		const res = await fetch(url);
 		if (!res.ok) {
 			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 		}
-
 		return await res.json();
 	};
 
@@ -266,6 +265,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 	const postData = async (url, data) => {
+
 		const res = await fetch(url, {
 			method: "POST",
 			headers: {
