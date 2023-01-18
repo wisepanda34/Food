ff34bef3ee5169f88aea05e613552d31f8e56563
tylkeen1803 <evghen.naydenko@gmail.com>
2023-01-18 23:21:48 +0200

    calc finished

--- js/script.js
+++ js/script.js
@@ -459,9 +459,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	// ==============  Calc  =======================
 	const result = document.querySelector('.calculating__result span');
-	let sex = 'female',
-		weight, height, age,
+	let sex, weight, height, age, ratio;
+
+	if (localStorage.getItem('sex')) {
+		sex = localStorage.getItem('sex');
+	} else {
+		sex = 'female';
+		localStorage.setItem('sex', 'female');
+	}
+
+	if (localStorage.getItem('ratio')) {
+		ratio = localStorage.getItem('ratio');
+	} else {
 		ratio = 1.375;
+		localStorage.setItem('retio', 1.375);
+	}
+
+	function initLocalSettings(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
+
+		elements.forEach(elem => {
+			elem.classList.remove(activeClass);
+			if (elem.getAttribute('id') === localStorage.getItem('sex')) {
+				elem.classList.add(activeClass);
+			}
+			if (elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+				elem.classList.add(activeClass);
+			}
+		})
+	}
+
+	initLocalSettings('#gender div', 'calculating__choose-item_active');
+	initLocalSettings('.calculating__choose_big div', 'calculating__choose-item_active');
+
 
 
 	function calcTotal() {
@@ -476,16 +506,20 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 	calcTotal();
 
-	function getStaticInformation(parentSelector, activeClass) {
-		const elements = document.querySelectorAll(`${parentSelector} div`);
+	function getStaticInformation(selector, activeClass) {
+		const elements = document.querySelectorAll(selector);
 
 		elements.forEach(elem => {
 			elem.addEventListener('click', (e) => {
 
 				if (e.target.getAttribute('data-ratio')) {
 					ratio = +e.target.getAttribute('data-ratio');
+
+					//ложим в память браузера выбранные пользователем параметры
+					localStorage.setItem('ratio', +e.target.getAttribute('data-ratio'));
 				} else {
 					sex = e.target.getAttribute('id');
+					localStorage.setItem('sex', e.target.getAttribute('id'));
 				}
 
 				calcTotal();
@@ -498,8 +532,8 @@ window.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 
-	getStaticInformation('#gender', 'calculating__choose-item_active');
-	getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+	getStaticInformation('#gender div', 'calculating__choose-item_active');
+	getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
 
 	function getDynamicInformation(selector) {
 
